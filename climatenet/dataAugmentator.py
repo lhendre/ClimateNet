b5b0a0b0efb3d4b0ab46ede980f49c14e062c81b
lhendre <lhendre2@github.com>
2022-12-01 18:58:49 -0800

    Correct update

--- climatenet/dataAugmentator.py
+++ climatenet/dataAugmentator.py
@@ -1,7 +1,8 @@
 import netCDF4 as nc
 import os
+import random
 
-data_path = '/Users/lucashendren/workspace/cs230/Data-4/Train'
+data_path = '/Users/lucashendren/workspace/cs230/Data-4/Test/test'
 dims_to_change = ["lon"]
 
 for filename in os.listdir(data_path):
@@ -13,7 +14,6 @@ for filename in os.listdir(data_path):
         continue
     if os.path.isfile(f):
         ds = nc.Dataset(f)
-
         print("org",ds)
 
         newDs = nc.Dataset(newF, "a", format="NETCDF4")
@@ -29,13 +29,18 @@ for filename in os.listdir(data_path):
             values = ds[name][:]
             if name in dims_to_change:
                 for i,val in enumerate(values):
-                    values[i]=val+1
+                    toAdd = random.randint(-180, 180)
+                    newVal=val+toAdd
+                    if newVal < -180:
+                        newVal= 360+newVal
+                    if newVal > 180:
+                        newVal= newVal-360
+                    values[i]=newVal
+
             newDs[name][:] = values
-            x[:] = values
             if name in dims_to_change:
                 print("old",ds.variables[name][0])
                 print("new",newDs.variables[name][0])
-        print(ds)
         print("new",newDs)
 
         newDs.close()

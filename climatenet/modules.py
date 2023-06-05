1f0eb13cd1fd7df7deb5a6bdc9ac8f552e2cb07c
lhendre <lhendre2@github.com>
2023-06-04 19:08:53 -0700

    WIP

--- climatenet/modules.py
+++ climatenet/modules.py
@@ -17,6 +17,30 @@ class Wrap(torch.nn.Module):
         # creating the circular padding
         return F.pad(x, (self.p,)*4 , mode='circular')
 
+class SelfAttention(nn.Module):
+    def __init__(self, nOut, reduction=16):
+        super(SelfAttention, self).__init__()
+        self.query = nn.Conv2d(nOut, nOut // reduction, kernel_size=1)
+        self.key = nn.Conv2d(nOut, nOut // reduction, kernel_size=1)
+        self.value = nn.Conv2d(nOut, nOut, kernel_size=1)
+        self.gamma = nn.Parameter(torch.zeros(1))
+
+    def forward(self, input):
+        batch_size, num_channels, height, width = input.size()
+
+        query = self.query(input).view(batch_size, -1, height * width).permute(0, 2, 1)
+        key = self.key(input).view(batch_size, -1, height * width)
+        value = self.value(input).view(batch_size, -1, height * width)
+
+        attention_weights = torch.matmul(query, key)
+        attention_weights = torch.softmax(attention_weights, dim=-1)
+
+        context = torch.matmul(attention_weights, value)
+        context = context.view(batch_size, -1, height, width)
+
+        output = self.gamma * context + input
+        return output
+
 class ConvBNPReLU(nn.Module):
     def __init__(self, nIn, nOut, kSize, stride=1):
         """
@@ -143,7 +167,7 @@ class ChannelWiseConv(nn.Module):
         output = self.padding(input)
         output = self.conv(output)
         return output
-      
+
 class DilatedConv(nn.Module):
     def __init__(self, nIn, nOut, kSize, stride=1, d=1):
         """
@@ -228,15 +252,18 @@ class ContextGuidedBlock_Down(nn.Module):
         """
         super().__init__()
         self.conv1x1 = ConvBNPReLU(nIn, nOut, 3, 2)  #  size/2, channel: nIn--->nOut
-        
+
         self.F_loc = ChannelWiseConv(nOut, nOut, 3, 1)
         self.F_sur = ChannelWiseDilatedConv(nOut, nOut, 3, 1, dilation_rate)
-        
+
         self.bn = nn.BatchNorm2d(2*nOut, eps=1e-3)
         self.act = nn.PReLU(2*nOut)
         self.reduce = Conv(2*nOut, nOut,1,1)  #reduce dimension: 2*nOut--->nOut
-        
-        self.F_glo = FGlo(nOut, reduction)    
+
+        # self.F_glo = nn.MultiheadAttention(nOut, num_heads=4, dropout=0.0)
+        # self.F_glo = SelfAttention(nOut, reduction)
+
+        self.F_glo = FGlo(nOut, reduction)
 
     def forward(self, input):
         output = self.conv1x1(input)
@@ -247,7 +274,7 @@ class ContextGuidedBlock_Down(nn.Module):
         joi_feat = self.bn(joi_feat)
         joi_feat = self.act(joi_feat)
         joi_feat = self.reduce(joi_feat)     #channel= nOut
-        
+
         output = self.F_glo(joi_feat)  # F_glo is employed to refine the joint feature
 
         return output
@@ -258,7 +285,7 @@ class ContextGuidedBlock(nn.Module):
         """
         args:
            nIn: number of input channels
-           nOut: number of output channels, 
+           nOut: number of output channels,
            add: if true, residual learning
         """
         super().__init__()
@@ -268,14 +295,17 @@ class ContextGuidedBlock(nn.Module):
         self.F_sur = ChannelWiseDilatedConv(n, n, 3, 1, dilation_rate) # surrounding context
         self.bn_prelu = BNPReLU(nOut)
         self.add = add
+        # self.F_glo = nn.MultiheadAttention(nOut, num_heads=4, dropout=0.0)
+        # self.F_glo = SelfAttention(nOut, reduction)
+
         self.F_glo= FGlo(nOut, reduction)
 
     def forward(self, input):
         output = self.conv1x1(input)
         loc = self.F_loc(output)
         sur = self.F_sur(output)
-        
-        joi_feat = torch.cat([loc, sur], 1) 
+
+        joi_feat = torch.cat([loc, sur], 1)
 
         joi_feat = self.bn_prelu(joi_feat)
 
@@ -295,4 +325,4 @@ class InputInjection(nn.Module):
     def forward(self, input):
         for pool in self.pool:
             input = pool(input)
-        return input   
+        return input
